68ad1d773da64c6b2114b3b5b21adbfa23246760
Luckyxg <87571231+GzGod@users.noreply.github.com>
2025-02-02 00:16:53 +0800

    Update banner.js

--- utils/banner.js
+++ utils/banner.js
@@ -1,6 +1,4 @@
-import chalk from 'chalk';
-
-const art = `
+const banner = `
                ╔═╗╔═╦╗─╔╦═══╦═══╦═══╦═══╗
                ╚╗╚╝╔╣║─║║╔══╣╔═╗║╔═╗║╔═╗║
                ─╚╗╔╝║║─║║╚══╣║─╚╣║─║║║─║║
@@ -11,19 +9,5 @@ const art = `
                我的gihub：github.com/Gzgod 本人仅做了汉化处理
                我的推特：推特雪糕战神@Hy78516012                  
                                  
-`;
-
-export function centerText(text) {
-    const lines = text.split('\n');
-    const terminalWidth = process.stdout.columns || 80; 
-    return lines
-        .map(line => {
-            const padding = Math.max((terminalWidth - line.length) / 2, 0);
-            return ' '.repeat(padding) + line;
-        })
-        .join('\n');
-}
-
-export function showBanner() {
-    console.log(chalk.green(centerText(art)));
-}
+                 `
+export default banner;
