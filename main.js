b86e606f16f8f962a21e66f50d7b9eb607951b85
Luckyxg <87571231+GzGod@users.noreply.github.com>
2025-02-02 00:08:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 import { readToken, delay } from "./utils/file.js";
 import { createConnection } from "./utils/websocket.js";
-import { showBanner } from "./utils/banner.js";
+import beddu from "./utils/banner.js";
 import { logger } from "./utils/logger.js";
 
 async function start() {
-    showBanner();
+    console.log(beddu);
     const tokens = await readToken("providers.txt");
     const proxies = await readToken("proxy.txt");
 
-    if (proxies.length < tokens.length) {
-        logger("代理数量不足以支持所有供应商。程序退出...");
-        return;
+    if (proxies.length === 0) {
+        logger("未找到代理 - 无代理运行中...", "", "warn");
     }
 
-    // 使用一个代理为每个令牌创建连接
+    // 使用每个token一个代理创建连接
     for (let i = 0; i < tokens.length; i++) {
         const token = tokens[i];
-        const proxy = proxies[i]; 
+        const proxy = proxies[i % proxies.length] || null;
 
         await createConnection(token, proxy);
-        await delay(5000);
+        await delay(1000);
     }
 }
 
