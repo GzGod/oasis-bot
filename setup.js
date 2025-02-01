a83aa49dcdf697497ed3a55c5e9027e385c30262
Luckyxg <87571231+GzGod@users.noreply.github.com>
2025-02-02 00:08:25 +0800

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,9 +1,10 @@
 import { readToken, delay } from "./utils/file.js";
-import { showBanner } from "./utils/banner.js";
+import beddu from "./utils/banner.js";
 import { loginFromFile } from "./utils/login.js";
 import { createProviders } from "./utils/providers.js";
 import { logger } from "./utils/logger.js";
 import { createInterface } from 'readline';
+import fs from 'fs';
 
 const rl = createInterface({
   input: process.stdin,
@@ -15,34 +16,42 @@ function askQuestion(query) {
 }
 
 async function setup() {
-  showBanner();
-  // 询问创建供应商的数量
-  const input = await askQuestion('请输入您想要创建的供应商数量 [1-100]: ');
+  console.log(beddu);
+  // 询问要创建的提供者数量
+  const input = await askQuestion('输入您想为每个账户创建的提供者数量 [1-100]: ');
   const numProv = parseInt(input, 10);
-  
+
   if (isNaN(numProv) || numProv < 1 || numProv > 100) {
-    logger("输入无效。请输入1到100之间的数字。", "", "error");
+    logger("无效输入。请输入1到100之间的数字。", "", "error");
+    rl.close();
+    return;
+  };
+
+  const accounts = await readToken("accounts.txt");
+  if (fs.existsSync('tokens.txt')) {
+    const tokens = await readToken("tokens.txt");
+
+    if (accounts.length !== tokens.length) {
+      fs.unlinkSync('tokens.txt');
+      const isLogin = await loginFromFile('accounts.txt');
+      if (!isLogin) {
+        logger("没有账户成功登录。退出...", "", "error");
+        rl.close();
+        return;
+      }
+    }
+  }
+
+  const isLogin = await loginFromFile('accounts.txt');
+  if (!isLogin) {
+    logger("没有账户成功登录。退出...", "", "error");
     rl.close();
     return;
   }
-  
-  const proxies = await readToken("proxy.txt");
-  const isLogin = await loginFromFile('accounts.txt');
 
-  if (proxies.length === 0) { 
-    logger('在proxy.txt中未找到任何代理。程序退出...', "", "error");
-    rl.close();
-    return; 
-  }
-  if (!isLogin) {
-    logger("没有账户成功登录。程序退出...", "", "error");
-    rl.close();
-    return; 
-  }
-
-  logger(`正在创建 ${numProv} 个供应商...`);
+  logger(`创建 ${numProv} 个提供者...`);
   await createProviders(numProv);
-  
+
   rl.close();
 }
 
